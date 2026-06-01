2119341dd0353a79347259714cdfead95cf12dbf
TANMAY CHAUDHARY <tanmaychaudhary11111@gmail.com>
2026-06-01 21:58:40 +0530

    multiplication table program in assignment_2.c

---
+++ Day_1/assignment_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n,i;
+    printf("enter the number to print table:");
+    scanf("%d",&n);
+    for ( i = 1; i <= 10; i++)
+    {
+       printf("%dx%d = %d\n",n,i,n*i);
+    }
+    
+    return 0;
+}
