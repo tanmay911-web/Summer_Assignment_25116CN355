fcb10a380a184341634130d7b8dbc70c41fae5d0
TANMAY CHAUDHARY <tanmaychaudhary11111@gmail.com>
2026-06-02 21:35:57 +0530

    Add program to find largest prime factor

---
+++ Day_5/assignment_20.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, largprime = 0, i;
+    printf("enter the number:");
+    scanf("%d", &n);
+    if (n <= 1)
+    {
+        printf("enter the number greater than 1");
+        return 0;
+    }
+
+    for (i = 2; i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            largprime = i;
+            n = n / i;
+        }
+    }
+    printf("the largest prime factor is %d", largprime);
+
+    return 0;
+}
