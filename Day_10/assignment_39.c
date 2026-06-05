15fba991bddc4df9197aa6433143ddb499bdd651
TANMAY CHAUDHARY <tanmaychaudhary11111@gmail.com>
2026-06-06 01:24:54 +0530

    Add C program to print pyramid pattern

---
+++ Day_10/assignment_39.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    printf("enter the no of rows:");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (int k = 1; k < n - i; k++)
+        {
+            printf(" ");
+        }
+        for ( j = 1; j <= i ; j++)
+        {
+            printf("%d ",j);
+        }
+        for (int a = i-1; a >=1; a--)
+        {
+           printf("%d ",a);
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
